d7782f6d68a9a1d59f8929a79a1a67324e7555fc
MiniCodehub <ironman102004@gmail.com>
2024-10-06 17:35:04 +0530

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_website/core/asset/app_images.dart';
 import 'package:portfolio_website/core/theme/app_colors.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,6 +16,16 @@ class _HomePageState extends State<HomePage> {
       body: Stack(
         children: [
           Align(alignment: Alignment.topCenter, child: _appBar(context)),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _homeContent(context),
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -81,6 +92,84 @@ class _HomePageState extends State<HomePage> {
           SizedBox(
             width: 100,
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _homeContent(BuildContext context) {
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: RichText(
+                  text: const TextSpan(
+                    text: "Hi, Its's ",
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                      fontSize: 50,
+                    ),
+                    children: [
+                      TextSpan(
+                          text: "Asfar",
+                          style: TextStyle(
+                              color: AppColors.primaryColor,
+                              fontFamily: "Poppins")),
+                    ],
+                  ),
+                ),
+              ),
+              RichText(
+                text: const TextSpan(
+                  text: "I'm a ",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                  children: [
+                    TextSpan(
+                        text: "App Developer",
+                        style: TextStyle(
+                            color: AppColors.primaryColor,
+                            fontFamily: "Poppins")),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 500,
+                child: Text(
+                  "An experienced app developer with a focus on creating highly scalable and user friendly mobile applications using Flutter. Skilled in integrating Firebase for real time database management, authentication, and cloud storage, ensuring seamless performance and enhanced security for users across platforms.",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            width: 200,
+          ),
+          Container(
+            decoration: const BoxDecoration(shape: BoxShape.circle, boxShadow: [
+              BoxShadow(blurRadius: 20, color: AppColors.primaryColor)
+            ]),
+            child: const CircleAvatar(
+              radius: 150,
+              backgroundImage: AssetImage(
+                AppImages.profile,
+              ),
+            ),
+          ),
         ],
       ),
     );
